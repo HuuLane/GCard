d6506bee619a372557799d45626d606ca51756e6
Huu Lane <huulane@gmail.com>
2020-12-27 14:06:26 +0800

    fix: clarify model template

--- update.js
+++ update.js
@@ -32,37 +32,69 @@ const invoke = function (action, version, params = {}) {
   })
 }
 
-const r = async path => fs.readFile(path, 'utf8')
+// return the contents of the specified file.
+const R = async path => fs.readFile(path, 'utf8')
 
-const main = async templateName => {
-  console.log(templateName)
+const replace = async modelName => {
+  // filepath
+  const f = path.join('.', modelName, 'Front.html')
+  const b = path.join('.', modelName, 'Back.html')
+  const s = path.join('.', modelName, 'styling.css')
 
-  const f = path.join('.', templateName, 'Front.html')
-  const b = path.join('.', templateName, 'Back.html')
-  const s = path.join('.', templateName, 'styling.css')
+  const tn = await getTemplateNames(modelName)
+  console.log(`Try to replace ${modelName}/${tn}`)
+
   invoke('updateModelTemplates', 6, {
     model: {
-      name: templateName,
+      name: modelName,
       templates: {
-        card1: {
-          Front: await r(f),
-          Back: await r(b)
+        [tn]: {
+          Front: await R(f),
+          Back: await R(b)
         }
       }
     }
   })
   invoke('updateModelStyling', 6, {
     model: {
-      name: templateName,
-      css: await r(s)
+      name: modelName,
+      css: await R(s)
     }
   })
 }
 
-try {
-  // main('GCard')
-  main('Gcard-Cloze') // can't update
-  // main('macmillan7000')
-} catch (err) {
-  console.error(err)
+// The return value means: which models can be replaced(or updated) from the working dir.
+const getLocalModelNames = async () => {
+  const modelNames = await invoke('modelNames', 6)
+  const localModelNames = []
+  for (const mn of modelNames) {
+    try {
+      await fs.access(path.join('.', mn))
+      localModelNames.push(mn)
+    } catch (error) {
+      // that's fine
+    }
+  }
+  return localModelNames
 }
+
+// Currently, all my models have only one template, generally named'card1', except the 'Cloze'.
+const getTemplateNames = async modelName => {
+  const result = await invoke('modelTemplates', 6, {
+    modelName
+  })
+  return Object.keys(result)[0]
+}
+
+;(async () => {
+  const mns = await getLocalModelNames()
+
+  for (const mn of mns) {
+    try {
+      replace(mn)
+    } catch (error) {
+      console.error(error)
+      process.exit(1)
+    }
+  }
+})()
